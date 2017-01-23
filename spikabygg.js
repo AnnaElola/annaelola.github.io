64a28de01d7848adb4dd75e0b9f71e2f9670f2be
Anna Elola <elola.anna@gmail.com>
2017-01-23 15:31:20 +0100

    Test remove footer

--- spikabygg.js
+++ spikabygg.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  var availW = window.screen.availWidth;
+  var availW = window.screen.width;
   console.log(availW);
   if (availW<860){
     $('#footer-middle').remove();
