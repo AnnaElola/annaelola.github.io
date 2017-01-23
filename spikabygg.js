a5561db7d44b21e8dbbc20b1996cca85e03ab41e
Anna Elola <elola.anna@gmail.com>
2017-01-23 13:59:55 +0100

    Lade till media queries

--- spikabygg.js
+++ spikabygg.js
@@ -1,12 +1,3 @@
 $(document).ready(function(){
-  /*  $('li').find('a').mouseenter(function(){
 
-         $(this).animate(
-        {
-          opacity: 0
-          /*  box-shadow: "0em 0.17em 0em white"
-        },
-        500
-    );
-  });*/
 });
