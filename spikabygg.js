5b7253784e84b3261c4ea7e165e31fca624f0dab
Anna Elola <elola.anna@gmail.com>
2017-01-25 15:31:04 +0100

    Layoutförbättring

--- spikabygg.js
+++ spikabygg.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
 
   var availW = window.screen.availWidth;
+  var pathname2 = $(location).attr('pathname');
+  var pathname = window.location.pathname;
+  var url      = window.location.href;
+  console.log(pathname);
+  console.log(pathname2);
+  console.log(url);
 
   if (availW<750){
     $('#footer-middle').remove();
     $('#menyButton').show();
-    
+    $('line').remove();
 
     $('#menyList').css("display", "none");
+
+
 
     $('#menyButton').click(function(){
       $('#menyList').slideToggle();
